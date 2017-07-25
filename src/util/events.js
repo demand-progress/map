be43a0c0f0dfde1fd14c2d01fd6403db157b54e6
Tibet Sprague <tibet.sprague@gmail.com>
2017-07-24 23:21:02 -0700

    Sort events by meetings at congressional offices at the top

--- src/util/events.js
+++ src/util/events.js
@@ -91,8 +91,14 @@ export function computeFilteredEvents(events, filters, zipcodes) {
 
       return distanceFromA - distanceFromB;
     });
-
   }
+
+  // Put the meetings at the congressional offices at the top of the list
+  filteredEvents.sort((a, b) => {
+    const aIsImportant = a.categories.indexOf('meeting') !== -1 ? 0 : 1;
+    const bIsImportant = b.categories.indexOf('meeting') !== -1 ? 0 : 1;
+    return aIsImportant - bIsImportant;
+  });
 
   return filteredEvents;
 }
